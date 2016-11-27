eb96b818e66eb270566d2c206b127e846179f3be
Арляпов Савелий <gragasv@gmail.com>
2016-11-28 07:12:48 +0800

    First item

--- app/src/main/java/app/android/sava/fifteen/MainActivity.java
+++ app/src/main/java/app/android/sava/fifteen/MainActivity.java
@@ -1,13 +1,17 @@
 package app.android.sava.fifteen;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
